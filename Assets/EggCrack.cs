193fa1da55e5d37d53e55ec674d9332e2f963193
oreotduck <oreonguyen2511@gmail.com>
2025-03-19 14:37:17 +0700

    EggCrack

--- Assets/EggCrack.cs
+++ Assets/EggCrack.cs
@@ -1,29 +1,41 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class EggCrack : MonoBehaviour
+public class EggCrack : MonoBehaviour, IPointerClickHandler
 {
 
     public Animator eggAnimator;
+    public BoxCollider2D Egg;
     public string letter;
     public GameObject letterImage;
     public int tapCount = 0;
     public int maxTap = 3;
+    private bool isCracked = false;
 
-    public void OnMouseDown()
+    public void Start()
     {
+        eggAnimator = GetComponent<Animator>();
+        Egg = GetComponent<BoxCollider2D>();
+        tapCount = 0;
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (isCracked) return;
+
+        Debug.Log("Egg Click!");
         tapCount++;
-        if (tapCount <= maxTap)
-        {
-            eggAnimator.SetTrigger("Crack");
-        }
+        eggAnimator.SetInteger("TapCount",tapCount);
 
         if (tapCount >= maxTap)
         {
+            isCracked=true;
             ShowLetter();
         }
     }
 
-    public void ShowLetter()
+    private void ShowLetter()
     {
         letterImage.SetActive(true);
         gameObject.SetActive(false);
--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
     public Animator transition;
-    public float transitionTime = 1f;
 
     // Update is called once per frame
     void Update()
